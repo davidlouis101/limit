c861f52938cb4867310fa4b96fc739b1dd0e1bbf
ALEONDEV <53395716+Leonxv@users.noreply.github.com>
2019-12-22 16:20:06 +0100

    Add files via upload

---
+++ src/Aleondev/Al/Main.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Aleondev\Al;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+
+class Main extends PluginBase{
+
+	public function onEnable() : void{
+		Server::getInstance()->getCommandMap()->unregister(Server::getInstance()->getCommand("version"));
+		$this->getLogger()->info("Hello World!");
+	}
+
+	public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
+		switch($command->getName()){
+			case "limit":
+                $sender->sendMessage("§e[GG]§7[§4Limit§7] §cGeneriert..");
+                $sender->sendMessage("§e[GG]§7[§4Limit§7] §cGeneriert...");
+                $sender->sendMessage("§e[GG]§7[§4Limit§7] §cGeneriert....");
+                $sender->sendMessage("§e[GG]§7[§4Limit§7] §cGeneriert.....");
+                $sender->sendMessage("§e[GG]§7[§4Limit§7] §cGeneriert......");
+                $sender->sendMessage("§e[GG]§7[§4Limit§7] §cGeneriert.......");
+                $sender->sendMessage("§e[GG]§7[§4Limit§7]§cDas Limit liegt bei");
+                $sender->sendMessage("§e[GG]§7[§4Limit§7] §e" .rand(1, 100000). "§e GC");
+
+				return true;
+			default:
+				return false;
+		}
+	}
+
+	public function onDisable() : void{
+		$this->getLogger()->info("Bye");
+	}
+}
